b822326f39547f82df21035fd14796ec10a1af64
LeandroCostPereira <63782482+LeandroCostPereira@users.noreply.github.com>
2020-05-04 16:58:34 +0100

    Add files via upload

---
+++ ficheiro02.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>   /* por causa da funçao exirt() */
+
+int main(){
+	FILE * fp;
+	char string[100];
+	int i;
+	fp = fopen("tmp/texto.txt", "w");
+	
+	if(!fp){
+		printf("Erro na abertura do arquivo");
+		exit(0);
+    }
+    
+    printf("Insira a string a gravar no ficheiro: ");
+    gets(string);
+    for(i=0;string[i];i++){
+       fputc(string[i],fp);
+    }
+    fclose(fp);
+    return 0;
+}
